7b4a4e6ddb1ad84b49483778af71cab853057f33
SuperBlockchainMan <talentdeveloper59@gmail.com>
2022-01-06 03:52:57 +0000

    Implement Box component

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
+import { TOPBAR_HEIGHT } from 'config'
+import { Flex } from 'components/Box'
+
+const StyledFooter = styled(Flex)`
+  min-height: ${TOPBAR_HEIGHT}px;
+  max-width: 1200px;
+  width: 100%;
+  justify-content: center;
+  margin: auto;
+  align-items: center;
+`
 
 const Footer: React.FC = () => {
   return (
-    <div>
+    <StyledFooter>
       Footer
-    </div>
+    </StyledFooter>
   )
 }
 
--- src/layouts/TopBar/index.tsx
+++ src/layouts/TopBar/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
 import { TOPBAR_HEIGHT } from 'config'
+import { Flex } from 'components/Box'
 
-const StyledTopBar = styled.div`
+const StyledTopBar = styled(Flex)`
   min-height: ${TOPBAR_HEIGHT}px;
+  max-width: 1200px;
+  width: 100%;
+  justify-content: center;
+  margin: auto;
+  align-items: center;
 `
 
 const TopBar: React.FC = () => {
