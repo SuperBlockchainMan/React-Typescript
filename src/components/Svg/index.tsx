331d64899980133382277a6d92dacda658b5d316
SuperBlockchainMan <talentdeveloper59@gmail.com>
2022-01-06 09:06:55 +0000

    Implement Svg component

---
+++ src/components/Svg/index.tsx
@@ -0,0 +1,5 @@
+export { default as ArrowBackIcon } from "./Icons/ArrowBack"
+export { default as CloseIcon } from "./Icons/Close"
+
+export { default as Svg } from "./Svg"
+export type { SvgProps } from "./types"
